1a6a61680d51b72efd20d64e8261e513711a7c3a
Fred <513317651@qq.com>
2018-01-29 00:21:00 +0800

    指针我来了

--- c.c
+++ c.c
@@ -1,110 +1,86 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define NUM_RANK 13
-#define NUM_SUIT 4
-#define NUM_CARD 5
-bool straight,flush,four,three;
-int pairs;
-char hand[NUM_CARD][2];
-void read_cards(char hand[NUM_CARD][2]);
-void analyze_hand(char hand[NUM_CARD][2]);
-void print_result(void);
-int main()
+#define MAX_DIGITS 10
+const bool segments[10][7]={
+	{true,true,true,true,true,true,false},
+	{false,true,true,false,false,false,false},
+	{true,true,false,true,true,false,true},
+	{true,true,true,true,false,false,true},
+	{false,true,true,false,false,true,true},
+	{true,false,true,true,false,true,true},
+	{true,false,true,true,true,true,true},
+	{true,true,true,false,false,false,false},
+	{true,true,true,true,true,true,true},
+	{true,true,true,true,false,true,true}
+};
+char digits[3][MAX_DIGITS*3];
+void clear_digits_array(void);
+void process_digit(int digit,int position);
+void print_digits_array(void);
+void main()
 {
-	for(;;)
+	char ch;
+	int position;
+	clear_digits_array();
+	for(position=0;position<MAX_DIGITS;)
 	{
-		read_cards(hand[NUM_CARD][2]);
-		analyze_hand(hand[NUM_CARD][2]);
-		void print_result(void);
-	}
-}
-void read_cards(void)
-{
-     int ncards=0,i;
-     bool bad_card,Delicate=false,judge[NUM_CARD]={false};
-     char ch_rank,ch_suit,ch;
-     while(ncards<NUM_CARD)
-     {
-     	bad_card=false;
-     	printf("Enter a card:");
-     	ch_rank=getchar();
-     	switch(ch_rank)
+		if((ch=getchar())!='\n')
 		{
-		case '0':         exit(EXIT_SUCCESS);
-		case '2':         ch_rank='2';break;
-		case '3':         ch_rank='3';break;
-		case '4':         ch_rank='4';break;
-		case '5':         ch_rank='5';break;
-		case '6':         ch_rank='6';break;
-		case '7':         ch_rank='7';break;
-		case '8':         ch_rank='8';break;
-		case '9':         ch_rank='9';break;
-		case 't':case 'T':ch_rank='t';break;
-		case 'j':case 'J':ch_rank='j';break;
-		case 'q':case 'Q':ch_rank='q';break;
-		case 'k':case 'K':ch_rank='k';break;
-		case 'a':case 'A':ch_rank='a';break;
-		default:          bad_card=true;
-		}    
-		ch_suit=getchar(); 
-		switch(ch_suit)
-		{
-		case 'c':case 'C':ch_suit='c';break;
-		case 'd':case 'D':ch_suit='d';break;
-		case 'h':case 'H':ch_suit='h';break;
-		case 's':case 'S':ch_suit='s';break;
-		default:          bad_card=true;
+			switch(ch)
+			{
+				case '0':process_digit(0,position);position++;break;
+				case '1':process_digit(1,position);position++;break;
+				case '2':process_digit(2,position);position++;break;
+				case '3':process_digit(3,position);position++;break;
+				case '4':process_digit(4,position);position++;break;
+				case '5':process_digit(5,position);position++;break;
+				case '6':process_digit(6,position);position++;break;
+				case '7':process_digit(7,position);position++;break;
+				case '8':process_digit(8,position);position++;break;
+				case '9':process_digit(9,position);position++;break;
+				default: break;
+			}
 		}
-		while((ch=getchar())!='\n')
-			if(ch!=' ') bad_card=true;
-	    if(bad_card)
-	    	printf("Bad cards;ignored.\n");
-	    for(i=0;i<ncards;i++)
-	    {
-	    	if(hand[i][0]==ch_rank&&hand[i][1]==ch_suit)
-	    		{
-	    			Delicate=true;
-	    			printf("Dulicate card; ignored.\n");
-	    	        break;
-	            }
-	    }
-	    if(!Delicate&&!bad_card)
-	    {
-            hand[ncards][0]=ch_rank;
-            hand[ncards][1]=ch_suit;
-	    }
-        ncards++;
+		else break;
 	}
+	print_digits_array();
 }
-void analyze_hand(void)
+void clear_digits_array(void)
 {
-	int ncards,n,times;
-	char ch,i;
-	three=four=flush=straight=false;
-	pairs=0;
-	//flush
-	for(ncards=1,ch=hand[0][1];ncards<NUM_CARD;ncards++)
+	int i,x;
+	for(x=0;x<3;x++)
+		for(i=0;i<MAX_DIGITS*3;i++)
+			digits[x][i]=' ';
+}
+void process_digit(int digit,int position)
+{
+	int i;
+	for(i=0;i<7;i++)
+	{
+		if(segments[digit][i]&&i%3==0)
 		{
-			if(hand[ncards][1]!=ch)
-				break;
-			else flush=true;
+			if(i==0)           digits[0][1+4*position]='-';
+			else if(i==6)      digits[1][1+4*position]='-';
+			else if(i==3)      digits[2][1+4*position]='-';
+			else ;
+		}
+		else if(segments[digit][i])
+		{
+			if(i==1)       digits[1][2+4*position]='|';
+			else if(i==5)  digits[1][4*position]='|'  ;
+			else if(i==2)  digits[2][2+4*position]='|';
+			else if(i==4)  digits[2][4*position]='|';
+		}
+	}
+
+}
+void print_digits_array()
+{
+	int i,j;
+	for(i=0;i<3;i++)
+		{
+			for(j=0;j<4*MAX_DIGITS;j++)
+			printf("%c",digits[i][j]);
+		printf("\n");
         }
-    //straight
-    for(ncards=1,i=hand[0][0];ncards<NUM_CARD;ncards++)
-    {
-    	if(hand[ncards][0]!=++i)
-    		break;
-    	else straight=true;
-    }
-    //4—of-a-kind
-    for(ncards=0;ncards<NUM_CARD-4;ncards++)
-    {
-    	ch=hand[ncards][0];
-        for(n=0;n<NUM_CARD;n++)
-        {
-        	if(hand[n][0]==ch&&n!=ncards)
-        		times++;
-        }
-    }
-    
+    printf("\n");
 }
