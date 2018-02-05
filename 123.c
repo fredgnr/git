691536467394faf9b8e873cc5f41158bf35bb6a5
Fred <513317651@qq.com>
2018-02-06 01:14:12 +0800

    djfk

--- 123.c
+++ 123.c
@@ -1,55 +1,79 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-int compute_vowel_count(char *word);
-int main(void)
+#include <stdio.h>
+#define STACK_SIZE 100
+int contents[STACK_SIZE];
+int top=0;
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(int i);
+int pop(void);
+int evaluate_RPN_expression(const char * expression)
+void main()
 {
-	char word[128];
-	printf("Enter a word: ");
-	scanf("%s", word);
-	printf("Scrabble value : %d", compute_vowel_count(word));
-	return 0;
-}
-int compute_vowel_count(char *word)
-{
-	int i, value = 0;
-	int N = strlen(word);
-	for (int i = 0; i < N; i++)
+	char input;
+	int i;
+	printf("Enter an RPN expression:");
+	for(;;)
 	{
-		word[i] = toupper(word[i]);
-	}
-	for (int i = 0; i < N; i++)
-	{
-		switch (word[i])
+		scanf(" %c",&input);
+		if(input>='0'&&input<='9')
 		{
-		case 'A': value += 1; break;
-		case 'B': value += 3; break;
-		case 'C': value += 3; break;
-		case 'D': value += 2; break;
-		case 'E': value += 1; break;
-		case 'F': value += 4; break;
-		case 'G': value += 2; break;
-		case 'H': value += 4; break;
-		case 'I': value += 1; break;
-		case 'J': value += 8; break;
-		case 'K': value += 5; break;
-		case 'L': value += 1; break;
-		case 'M': value += 3; break;
-		case 'N': value += 1; break;
-		case 'O': value += 1; break;
-		case 'P': value += 3; break;
-		case 'Q': value += 10; break;
-		case 'R': value += 1; break;
-		case 'S': value += 1; break;
-		case 'T': value += 1; break;
-		case 'U': value += 1; break;
-		case 'V': value += 4; break;
-		case 'W': value += 4; break;
-		case 'X': value += 8; break;
-		case 'Y': value += 4; break;
-		case 'Z': value += 10; break;
-		default:;
+			switch(input)
+			{
+			case '0': i=0;break;
+			case '1': i=1;break;
+			case '2': i=2;break;
+			case '3': i=3;break;
+			case '4': i=4;break;
+			case '5': i=5;break;
+			case '6': i=6;break;
+			case '7': i=7;break;
+			case '8': i=8;break;
+			case '9': i=9;break;
+			}
+			push(i);
 		}
+		else if(input=='+')
+			push(pop()+pop());
+		else if(input=='*')
+			push(pop()*pop());
+		else if(input=='-')
+			push(-pop()+pop());
+		else if(input=='/')
+			push((int)(1/((double)pop()/(double)pop())));
+		else if(input=='=')
+		{
+			printf("%d\n",pop());
+			printf("Enter an RPN expression:");
+			make_empty();
+		}
+		else  return;
+
 	}
-	return value;
+}
+void make_empty(void)
+{
+	top=0;
+}
+bool is_empty(void)
+{
+	return top==0;
+}
+bool is_full(void)
+{
+	return top==STACK_SIZE;
+}
+void push(int i)
+{
+	if(is_full())
+		printf("Expression is too complex\n");
+	else 
+		contents[top++]=i;
+}
+int pop(void)
+{
+	if(is_empty())
+		printf("Not enough operands in the expression\n");
+	else 
+		return contents[--top];
 }
